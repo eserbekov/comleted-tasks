5bcf3a4f9b3bd59ad1339fa7090af9f5abf3abd0
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-02-14 23:33:17 +0200

    Solutions using array methods.

---
+++ arrayMethods.js
@@ -0,0 +1,16 @@
+// 8 kyu. Beginner - Lost Without a Map
+
+const maps = x => x.map(el => el*2);
+
+// 8 kyu. Enumerable Magic #25 - Take the First N Elements
+
+function take(arr, n) {
+    return arr.splice(0,n);
+}
+
+// 8 kyu. Remove First and Last Character Part Two
+
+function array(arr){
+    return arr.split(',').slice(1, -1).join(' ') || null
+}
+
