a23f051f65da2e93c38d632e14be13bde9a72f37
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-02-15 23:41:14 +0200

    Add solution 8 kyu. Is this my tail?

--- stringLength.js
+++ stringLength.js
@@ -3,3 +3,14 @@
 function getChar(c){
     return String.fromCodePoint(c)
 }
+
+//8 kyu. Is this my tail?
+
+function correctTail(bod, tail) {
+    let sub = bod.substr(bod.length-(tail.length))
+    if (sub === tail) {
+        return true
+    }
+    else
+        return false
+}
