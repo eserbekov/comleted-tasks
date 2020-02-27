5b7cba3e2bd3cb353641ae04ef73361c8c3fda6e
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-02-27 22:18:26 +0200

    Add solution 7 kyu. Mispelled word

---
+++ CodeWars/7 kyu_Mispelled word.js
@@ -0,0 +1,19 @@
+//7 kyu. Mispelled word
+
+const mispelled = (word1, word2) => {
+    if (word1.length < word2.length){
+        let temp = word1;
+        word1 = word2;
+        word2 = temp;
+    }
+    let count = 0;
+    for (let i = 0,j = 0; i < word1.length; i++, j++){
+        if (word1[i] !== word2[j]){
+            count++
+            if (word1.length > word2.length){
+                i++
+            }
+        }
+    }
+    return count<2
+}
