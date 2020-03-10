79e06c35d7e2b4be592115cbbef9b37d06c085ff
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-03-10 23:30:56 +0200

    8 kyu. Squash the bugs

---
+++ CodeWars/8 kyu_Squash the bugs.js
@@ -0,0 +1,12 @@
+// 8 kyu. Squash the bugs
+
+function findLongest(str) {
+    const spl = str.split(" ");
+    let longest = 0
+    for (let i = 0; i < spl.length; i++) {
+        if(spl[i].length > longest) {
+            longest = spl[i].length
+        }
+    }
+    return longest
+}
