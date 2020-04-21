c90019c3b0ebba1c4e439e92c030de8b0258aad6
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-04-21 21:32:06 +0200

    Add 8 kyu. Multiply and 8 kyu. Keep Hydrated

---
+++ CodeWars/8_kyu/8 kyu Keep Hydrated!.js
@@ -0,0 +1,6 @@
+// 8 kyu. Keep Hydrated!
+
+function litres(time) {
+    let water = Math.floor(time * 0.5);
+    return water;
+}
---
+++ CodeWars/8_kyu/8 kyu Multiply.js
@@ -0,0 +1,5 @@
+// 8 kyu. Multiply
+
+function multiply(a, b){
+    return a * b;
+}
